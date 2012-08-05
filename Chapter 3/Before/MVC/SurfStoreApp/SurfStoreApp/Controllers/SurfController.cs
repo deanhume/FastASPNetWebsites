d15b786983290cb6cc17030c3a3e812cc9582406
Dean Hume <deanhume@gmail.com>
2012-08-05 13:17:39 +0100

    Added initial "before" projects - Chapter 3

---
+++ Chapter 3/Before/MVC/SurfStoreApp/SurfStoreApp/Controllers/SurfController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Web.Mvc;
+using SurfStoreApp.Models;
+using SurfStoreApp.Utils;
+
+namespace SurfStoreApp.Controllers
+{
+    public class SurfController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Product(string category)
+        {
+            List<ProductModel> productModel = new List<ProductModel>();
+
+            // Check if a category was passed in first.
+            if (!string.IsNullOrWhiteSpace(category))
+            {
+                // Retrieve the product images
+                Utils.ImageUtils imageUtils = new ImageUtils();
+                var productImages = imageUtils.RetrieveProductImages(category);
+
+                // Loop through and add to our Model
+                foreach (FileInfo productImage in productImages)
+                {
+                    string imagePath = "~/Content/Images/" + category + "/" + productImage.Name;
+
+                    productModel.Add(new ProductModel { ImageDescription = productImage.Name.Replace(productImage.Extension, ""), ImageUrl = imagePath });
+                }
+            }
+
+            return View(productModel);
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
